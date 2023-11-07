41b98a14df9be742f4610b3967933bcf21936de6
Ramona <r.ingabire@alustudent.com>
2023-11-07 03:23:13 -0800

    Task 2

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,7 @@
 def uniq_add(my_list=[]):
     summation = 0
     new_list = sorted(my_list)
-    for in in range(len(my_list)):
-        for i == 0 or new_list[i] != new_list[i -1]:
+    for i in range(len(new_list)):
+        if i == 0 or new_list[i] != new_list[i - 1]:
             summation += new_list[i]
-            return(summation)
+    return(summation)
