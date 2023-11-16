c881d9c41f76339d027f1ad7a4a6fe979414e35c
Ramona <r.ingabire@alustudent.com>
2023-11-16 07:46:01 -0800

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,8 +8,13 @@ if __name__ == "__main__":
         __import__('6-load_from_json_file').load_from_json_file
 
     try:
+        # Load items from JSON file
         items = load_from_json_file("add_item.json")
     except FileNotFoundError:
-        items = []
+        items = []  # If the file doesn't exist, start with an empty list
+
+    # Extend the items list with command-line arguments (excluding the script name)
     items.extend(sys.argv[1:])
+
+    # Save the updated list back to the JSON file
     save_to_json_file(items, "add_item.json")
